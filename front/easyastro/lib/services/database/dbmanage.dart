6e58ae988e4ad136c5d69e016de001fda8470f46
Erwan <me@me.com>
2025-05-29 17:00:08 +0200

    lib update

--- front/easyastro/lib/services/database/dbmanage.dart
+++ front/easyastro/lib/services/database/dbmanage.dart
@@ -1,11 +1,9 @@
 import 'package:csv/csv.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:easyastro/models/catalogs.dart';
 import 'package:easyastro/astro/astrocalc.dart';
 import 'package:sweph/sweph.dart';
 import 'package:easyastro/services/database/globals.dart';
-import 'package:path_provider/path_provider.dart';
 import "package:easyastro/services/utils/filereader.dart";
 
 double raToDouble(String ra) {
